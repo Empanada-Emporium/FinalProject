41ecd6d1ed636de3972b9304f88fc7af5feceda1
JeszCode <104606699+JeszCode@users.noreply.github.com>
2023-03-21 19:45:57 -0400

    Add files via upload
    Added some JS and HTML

---
+++ HTML and CSS/Data.js
@@ -0,0 +1,37 @@
+// Shop DATA
+
+let shopItemsData = [{
+    id: "beef",
+    name: "Beef Empanada",
+    price: 1.94,
+    desc: "Filled with our delicious seasoned beef, in a sauce made with tomatos, onions, and garlic.",
+    img: "images/Beef.jpg"
+},
+{
+    id: "chicken",
+    name: "Chicken Empanada",
+    price: 1.94,
+    desc: "Filled with mouth watering shredded chicken, in a sauce made with tomatos, onions, and garlic.",
+    img: "images/Chicken.jpg"
+},
+{
+    id: "pork",
+    name: "Pork Empanada",
+    price: 2.36,
+    desc: "Filled with our juicy pulled pork, in a sauce made with tomatos, onions, and garlic.",
+    img: "images/Pork.jpg"
+},
+{
+    id: "spinchess",
+    name: "Spinach and Cheese Empanada",
+    price: 1.86,
+    desc: "Pastry filled with fresh spinach, and gooey Queso asadero.",
+    img: "images/Spinach_and_cheese.jpg"
+},
+{
+    id: "vegetable",
+    name: "Vegetable Empanada",
+    price: 1.94,
+    desc: "Our vegetable medley! Filled with black beans, veggies, corn and our secret blend of spices.",
+    img: "images/Vegetable.jpg"
+}];
---
+++ HTML and CSS/cart.js
@@ -0,0 +1,143 @@
+let label = document.getElementById('label') // declaring block scoped variable
+let shoppingCart = document.getElementById('shopping-cart') // declaring block scoped variable
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+// Caculation function to calculate the amount of item selected
+
+let Calculation =()=>{
+    let cartIcon = document.getElementById("cartAmount"); // Target Id = "cartAmount" in Html
+    cartIcon.innerHTML =  basket.map((x)=>x.item).reduce((x, y) => x + y ,0);
+       
+    };
+    
+    Calculation ();
+    
+    let generateCartItem = ()=>{
+        if(basket.length !==0){
+            return (shoppingCart.innerHTML = basket.map((x)=>{
+                let {id, item} = x;
+                let search = shopItemsData.find((y)=> y.id === id) || [];
+                let {img, name, price}= search
+
+                return `
+                <div class="cart-item">
+                <img width="100" height="120"  src=${img} alt=" five Empanada product">
+                <div class="details">
+
+                <div class="title-price-x">
+                    <h4 class="title-price">
+                    <p>${name}</p>
+                    <p class ="cart-item-price">$${price}</p>
+                    </h4>
+                    <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
+                </div>
+
+                <div class="buttons_cart">
+							<i onclick = "decrement(${id})" class="bi bi-dash-lg"></i>
+							<div id=${id} class="quantity">${item}</div>
+							<i onclick = "increment(${id})" class="bi bi-plus-lg"></i>
+                            
+				</div>
+
+                <h3 class="cart_price">$ ${item * search.price}</h3>
+                </div>    
+                </div>
+                `;
+            }).join(""));
+        }
+        else{
+            shoppingCart.innerHTML =``;
+            label.innerHTML = `
+            <h3> Cart is Empty</h3>
+            <a href="menu.html">
+            <button class="MenuBtn"> Back to the Menu</button>
+            </a>
+            `;
+        }
+
+    };
+
+    generateCartItem();
+
+    let increment = (id) => {
+        let selectedItem = id; // Assigning the variable id to selectedItem
+    
+        // search for item and pushing it to the basket
+        let search = basket.find((x)=> x.id === selectedItem.id);
+    
+        if( search === undefined){
+            basket.push(
+            {
+                id: selectedItem.id,
+                item: 1,
+            }
+        );
+    
+        } 
+        else{
+            search.item += 1;
+        }
+        localStorage.setItem("data", JSON.stringify(basket)); //Saving data in a local storage
+        generateCartItem();
+        update(selectedItem.id);
+    };
+    
+    // Decreasing the item function
+    let decrement = (id) => {
+        let selectedItem = id; // Assigning the variable id to selectedItem
+    
+        // search for item and pushing it to the basket
+        let search = basket.find((x)=> x.id === selectedItem.id);
+        if (search === undefined) return;
+        else if( search.item === 0) return; // end if item equal zero
+        else{
+            search.item -= 1;
+        }
+        update(selectedItem.id); 
+        basket = basket.filter((x)=>x.item !==0); // this is to make sure the item does not store zero item in the array.
+        generateCartItem();
+        localStorage.setItem("data", JSON.stringify(basket)); //Saving data in a local storage
+    };
+    
+    // making the number in the item count work.
+    let update = (id) => {
+        let search = basket.find((x)=>x.id === id);
+        document.getElementById(id).innerHTML = search.item
+        Calculation();
+        TotalAmount();
+    };
+
+    //removing item from the cart
+    let removeItem =(id) => {
+        let selectedItem = id;
+        basket = basket.filter((x)=>x.id !== selectedItem.id);
+        generateCartItem();
+        TotalAmount();
+        Calculation();
+        localStorage.setItem("data", JSON.stringify(basket));
+    };
+
+    let clearCart = ()=>{
+        basket = []
+        generateCartItem();
+        Calculation();
+        localStorage.setItem("data", JSON.stringify(basket)); //Saving data in a local storage
+    }
+    // Calculating Total amount
+    let TotalAmount = () =>{
+        if(basket.length !==0){
+            let amount = basket.map((x)=>{
+               let {item, id} =x; 
+               let search = shopItemsData.find((y)=> y.id === id) || [];
+               return item * search.price;
+            }). reduce((x,y)=>x+y,0)
+           label.innerHTML = `
+           <h3> Total Bill : $ ${amount}</h3>
+           <button class ="checkout">Checkout</button>
+           <button onclick="clearCart()" class="removeall">Clear Cart</button>
+           `
+        } else return;
+
+    } ;
+     
+    TotalAmount();
