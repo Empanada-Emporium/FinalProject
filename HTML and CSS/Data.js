26cf5898627b1ba678ba23a2461801e8fd02e838
Demitri Malher <104509147+Grimpick213@users.noreply.github.com>
2023-03-28 19:07:30 -0400

    Add files via upload
    Replacement Directory for current code.

---
+++ HTML and CSS/Data.js
@@ -0,0 +1,37 @@
+// Shop DATA
+
+let shopItemsData = [{
+    id: "beef",
+    name: "Beef Empanada",
+    price: 1.94,
+    desc: "Indulge in the savory goodness of our seasoned beef, perfectly cooked and bathed in a luscious sauce made with vine-ripened tomatoes, aromatic onions, and garlic. Each bite is a heavenly explosion of flavors that will leave your taste buds begging for more.",
+    img: "images/Beef.jpg"
+},
+{
+    id: "chicken",
+    name: "Chicken Empanada",
+    price: 1.94,
+    desc: "Tantalize your taste buds with our mouth-watering shredded chicken empanada, slow-cooked to perfection and served in a savory sauce made with juicy tomatoes, caramelized onions, and garlic. The flavors and textures are perfectly balanced, delivering a truly unforgettable gastronomic experience that will leave you craving for more.",
+    img: "images/Chicken.jpg"
+},
+{
+    id: "pork",
+    name: "Pork Empanada",
+    price: 2.36,
+    desc: "Our juicy pulled pork empanada, slow-cooked to perfection and smothered in a delectable sauce made with succulent tomatoes, caramelized onions, and fresh garlic. Each bite is a harmonious blend of textures and flavors, making it a culinary delight that will take your taste buds on a journey to paradise.",
+    img: "images/Pork.jpg"
+},
+{
+    id: "spinchess",
+    name: "Spinach and Cheese Empanada",
+    price: 1.86,
+    desc: "Experience the perfect balance of flavors and textures with our empanada pastry filled with farm-fresh spinach and oozy Queso Asadero. Bite into the buttery, flaky crust and savor the delectable blend of earthy spinach and melted cheese, creating a wholesome and satisfying meal that will leave you feeling nourished and satisfied.",
+    img: "images/Spinach_and_cheese.jpg"
+},
+{
+    id: "vegetable",
+    name: "Vegetable Empanada",
+    price: 1.94,
+    desc: "Experience a burst of flavor in every bite with our vegetable medley empanada, a delicious blend of savory black beans, crisp veggies, and sweet corn, seasoned with our secret blend of spices. Every mouthful is a sensory explosion of texture, taste, and aroma, making it a satisfying and wholesome meal that will leave you feeling energized and satisfied. Indulge in this guilt-free pleasure, and let the goodness of our natural ingredients nourish your body and soul.",
+    img: "images/Vegetable.jpg"
+}];
